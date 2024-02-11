94132d23f668c7c98c3fd07c5627f76feacbb752
KetchupLardon <axel.gudolle@gmail.com>
2024-02-11 18:43:55 +0100

    refactor MainNavigation

--- frontend/src/components/MainNavigation.jsx
+++ frontend/src/components/MainNavigation.jsx
@@ -1,32 +1,29 @@
 import { NavLink } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
 
+const NAVIGATION_SETTINGS = [
+  { path: "/", text: "Home" },
+  { path: "/events", text: "Events" },
+];
+
 export const MainNavigation = () => {
   return (
     <header className={classes.header}>
       <nav>
         <ul className={classes.list}>
-          <li>
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-              end
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/events"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              Events
-            </NavLink>
-          </li>
+          {NAVIGATION_SETTINGS.map(({ path, text }, index) => (
+            <li>
+              <NavLink
+                to={path}
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+                end={index === 0}
+              >
+                {text}
+              </NavLink>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
